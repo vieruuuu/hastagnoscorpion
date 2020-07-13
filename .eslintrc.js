1988ccd4b1ae1b41784547c7c7ef88106737181d
vieru <mixerandr3w001@gmail.com>
2020-07-13 20:08:54 +0300

    am adaugat config pt prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ["standard", "prettier"],
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  rules: {
-  }
-}
+  rules: {},
+};
