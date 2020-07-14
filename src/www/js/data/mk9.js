73d5e9be77aa727c44b4703b8af8316891293c12
vieru <mixerandr3w001@gmail.com>
2020-07-14 13:52:48 +0300

    am pus cum ar trebui sa fie

--- src/www/js/data/mk9.js
+++ src/www/js/data/mk9.js
@@ -4,8 +4,8 @@ import { Caracter } from "../typedefs.js";
  * lista de caractere mk9
  */
 export const mk9 = [
-  new Caracter("Scorpion1"),
-  new Caracter("Scorpion2"),
+  new Caracter("Scorpion", "/mk9/scorpion.png"),
+  new Caracter("Sub-Zero", "/mk9/sub-zero.png"),
   new Caracter("Scorpion3"),
   new Caracter("Scorpion4"),
   new Caracter("Scorpion5"),
