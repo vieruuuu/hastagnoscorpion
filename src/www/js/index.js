50c06f645a9b6aa7932fffc3bba85bbb328a49ce
vieru <mixerandr3w001@gmail.com>
2020-07-14 14:09:40 +0300

    pentru eslint

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { Caracter } from "./typedefs.js";
+// eslint-disable-next-line no-unused-vars
 import { getRandom, render } from "./functions.js";
 import { Caractere } from "./data/index.js";
 
