9137bd3bcf9188e72c6f3da418798aa2526f6620
vieru <mixerandr3w001@gmail.com>
2020-07-14 13:52:17 +0300

    am adaugat functia render

--- src/www/js/functions.js
+++ src/www/js/functions.js
@@ -30,3 +30,22 @@ export function getRandom(Caractere) {
     Caractere: CaractereNoi,
   };
 }
+
+/**
+ * afiseaza o lista de caractere folosind un template
+ * @param {HTMLElement} element elementul in care afiseze caracterele
+ * @param {Caracter[]} data un array cu caractere
+ * @param {function(Caracter): string} template ia un Caracter si returneaza ce html ar trebui sa se afiseze
+ * @returns {Void}
+ */
+export function render(element, data, template) {
+  let renderedData = "";
+
+  for (let i = 0; i < data.length; i++) {
+    const caracter = data[i];
+
+    renderedData += template(caracter);
+  }
+
+  element.innerHTML = renderedData;
+}
